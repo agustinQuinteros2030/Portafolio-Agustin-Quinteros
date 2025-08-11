9d1f4a83ef960a8b0a7a0851e700f9c1112ed87c
AgustinQuinteros2030 <agustinquinteros84@gmail.com>
2025-08-11 14:40:30 -0300

    Creación del proyecto MVC

---
+++ cv_quinteros_Agustin/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using cv_quinteros_Agustin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cv_quinteros_Agustin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
